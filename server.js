9006392cfae407dc246e992fff26ae55443252c8
JAKKRIT <jakkritchaichan94@gmail.com>
2026-03-04 21:36:44 +0700

    fix render proxy session

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/uploads", express.static("uploads"));
-
+app.set("trust proxy", 1);
 app.use(
   session({
     name: "nail-session",
