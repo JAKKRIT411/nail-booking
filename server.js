a2699a37077fb782c27ff791b1230d926d034863
JAKKRIT <jakkritchaichan94@gmail.com>
2026-03-04 19:09:33 +0700

    fix session store

--- server.js
+++ server.js
@@ -518,5 +518,5 @@ process.on("unhandledRejection", (err) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("🚀 Server running on port " + PORT);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
